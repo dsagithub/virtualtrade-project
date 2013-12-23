fdd022465f4d5de7af7f59a9e0d3477439d9aa4d
Joan Ignasi <joan.ignasi.florit@estudiant.upc.edu>
2013-12-23 13:25:13 +0100

    UserResource y función getusers

---
+++ virtualtrade-server/virtualtrade-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g1/virtualtrade/api/UserResource.java
@@ -0,0 +1,91 @@
+package edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.api;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriInfo;
+
+import edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.model.User;
+import edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.model.UserCollection;
+import edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.model.VirtualRootAPI;
+
+@Path("/users")
+public class UserResource {
+
+	@Context
+	private UriInfo uriInfo;
+	@Context
+	private SecurityContext security;
+	private DataSource ds = DataSourceSPA.getInstance().getDataSource();
+	VirtualRootAPI root = new VirtualRootAPI();
+	String rel = null;
+
+	@GET
+	@Produces(MediaType.VIRTUAL_API_USER_COLLECTION)
+	public UserCollection getUsers() {
+		UserCollection users = new UserCollection();
+
+		// TODO: Retrieve all stings stored in the database, instantiate one
+		// Sting for each one and store them in the StingCollection.
+		Connection conn = null;
+		Statement stmt = null;
+		String sql;
+
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServiceUnavailableException(e.getMessage());
+		}
+
+		try {
+			stmt = conn.createStatement();
+			sql = "SELECT * FROM users";
+
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				User user = new User();
+				user.setEmail(rs.getString("email"));
+				user.setName(rs.getString("name"));
+				user.setPhoto(rs.getString("photo"));
+				user.setProvincia(rs.getString("provincia"));
+				user.setCiudad(rs.getString("ciudad"));
+				user.setCalle(rs.getString("calle"));
+				user.setNumero(rs.getInt("numero"));
+				user.setPiso(rs.getInt("piso"));
+				user.setPuerta(rs.getInt("puerta"));
+				user.setBanned(rs.getBoolean("banned"));
+				user.setUserpass(rs.getString("userpass"));
+				user.setLastModified(rs.getDate("lastModified"));
+			
+				users.add(user);
+			}
+		} catch (SQLException e) {
+			throw new InternalServerException(e.getMessage());
+		}
+
+		finally {
+			try {
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return users;
+	}
+	
+}
--- virtualtrade-server/virtualtrade-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g1/virtualtrade/model/UserCollection.java
+++ virtualtrade-server/virtualtrade-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g1/virtualtrade/model/UserCollection.java
@@ -2,13 +2,34 @@ package edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.links.Link;
+
 public class UserCollection {
 	private List<User> users = new ArrayList<User>();
-	
+	private List<Link> links = new ArrayList<Link>();
+
+	public void add(User user) {
+		users.add(user);
+	}
+
 	public List<User> getUsers() {
 		return users;
 	}
+
 	public void setUsers(List<User> users) {
 		this.users = users;
+	}
+	
+
+	public void add(Link link) {
+		links.add(link);
+	}
+
+	public List<Link> getLinks() {
+		return links;
+	}
+
+	public void setLinks(List<Link> links) {
+		this.links = links;
 	}
 }

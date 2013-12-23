406a1894d059fa78fe665778b821c5ae1dadc78d
Joan Ignasi <joan.ignasi.florit@estudiant.upc.edu>
2013-12-23 12:29:25 +0100

    añadido MediaType

---
+++ virtualtrade-server/virtualtrade-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g1/virtualtrade/api/MediaType.java
@@ -0,0 +1,12 @@
+package edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.api;
+
+public interface MediaType {
+	
+	public final static String VIRTUAL_API_USER = "application/vnd.virtual.api.user+json";
+	public final static String VIRTUAL_API_USER_COLLECTION = "application/vnd.virtual.api.user.collection+json";
+	
+	public final static String VIRTUAL_API_ERROR = "application/vnd.dsa.virtual.error+json";
+	public final static String VIRTUAL_API_LINK = "application/vnd.dsa.virtual.link+json";
+	public final static String VIRTUAL_API_LINK_COLLECTION = "application/vnd.dsa.virtual.link.collection+json";
+	
+}

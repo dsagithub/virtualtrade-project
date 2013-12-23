5cb3a19f5653844db1bf736f7c8834c78beb5c4e
Joan Ignasi <joan.ignasi.florit@estudiant.upc.edu>
2013-12-23 12:58:13 +0100

    añadido el paquete links y las tres clases

---
+++ virtualtrade-server/virtualtrade-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g1/virtualtrade/links/Link.java
@@ -0,0 +1,41 @@
+package edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.links;
+
+public class Link {
+
+	private String uri;
+	private String rel; // indica la relación que tiene el recurso
+	private String type; // tipo de media que responde o consume esa uri
+	private String title; // descripción legible de lo que hay en la uri
+
+	public String getUri() {
+		return uri;
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+
+	public String getRel() {
+		return rel;
+	}
+
+	public void setRel(String rel) {
+		this.rel = rel;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
---
+++ virtualtrade-server/virtualtrade-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g1/virtualtrade/links/VirtualAPILinkBuilder.java
@@ -0,0 +1,6 @@
+package edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.links;
+
+public class VirtualAPILinkBuilder {
+
+	
+}

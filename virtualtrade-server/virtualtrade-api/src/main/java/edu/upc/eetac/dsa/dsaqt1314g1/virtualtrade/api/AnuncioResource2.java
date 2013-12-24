580e05ed83d990a4a4e6ea51d00ae95e3dbf292f
arnau.garcia.casals <arnau193@gmail.com>
2013-12-24 18:06:03 +0100

    GET anuncios por length y offset a medias

--- virtualtrade-server/virtualtrade-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g1/virtualtrade/api/AnuncioResource2.java
+++ virtualtrade-server/virtualtrade-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g1/virtualtrade/api/AnuncioResource2.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.sql.DataSource;
 import javax.ws.rs.Consumes;
@@ -15,11 +17,13 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
 
 import edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.model.Anuncio;
+import edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.model.AnuncioCollection;
 import edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.model.Imagen;
 import edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.model.ImagenCollection;
 import edu.upc.eetac.dsa.dsaqt1314g1.virtualtrade.model.VirtualRootAPI;
@@ -315,5 +319,112 @@ public class AnuncioResource2 {
 		}
 		return anuncio;
 	}
+	
+	
+	
+	
+	@GET
+	@Produces(MediaType.VIRTUAL_API_ANUNCIO_COLLECTION)
+	public AnuncioCollection getAnuncios(@QueryParam("offset") String offset,
+			@QueryParam("length") String length) {
+
+		if ((offset == null) || (length == null))
+			throw new BadRequestException(
+					"offset and length are mandatory parameters");
+		int ioffset, ilength;
+		try {
+			ioffset = Integer.parseInt(offset);
+			if (ioffset < 0)
+				throw new NumberFormatException();
+		} catch (NumberFormatException e) {
+			throw new BadRequestException(
+					"offset must be an integer greater or equal than 0.");
+		}
+		try {
+			ilength = Integer.parseInt(length);
+			if (ilength < 1)
+				throw new NumberFormatException();
+		} catch (NumberFormatException e) {
+			throw new BadRequestException(
+					"length must be an integer greater or equal than 1.");
+		}
+
+		AnuncioCollection anuncios = new AnuncioCollection();
+
+		// TODO: Retrieve all stings stored in the database, instantiate one
+		// Sting for each one and store them in the StingCollection.
+		Connection conn = null;
+		Statement stmt = null;
+		String sql,sql2;
+
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServiceUnavailableException(e.getMessage());
+		}
+
+		try {
+			stmt = conn.createStatement();
+
+
+				sql = "SELECT * FROM anuncio ORDER BY creation_timestamp LIMIT "
+						+ offset + "," + length + "";
+
+
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				Anuncio anuncio = new Anuncio();
+
+				
+				anuncio.setAnuncioid(rs.getInt("anuncioid"));
+				anuncio.setEmail(rs.getString("email"));
+				anuncio.setSubject(rs.getString("subject"));
+				anuncio.setContent(rs.getString("content"));
+				anuncio.setEstado(rs.getBoolean("estado"));
+				anuncio.setPrecio(rs.getInt("precio"));
+				anuncio.setCreation_timestamp(rs
+						.getTimestamp("creation_timestamp"));
+				anuncio.setAtributo1(rs.getString("atributo1"));
+				anuncio.setAtributo2(rs.getString("atributo2"));
+				anuncio.setAtributo3(rs.getString("atributo3"));
+				anuncio.setMarca(rs.getString("marca"));
+				
+//				sql2= "SELECT * FROM imagen WHERE anuncioid='"+anuncio.getAnuncioid()+"'";
+//				ResultSet rs2=stmt.executeQuery(sql);
+//
+//				while (rs2.next()) {
+//					Imagen imagen = new Imagen();
+//					imagen.setUrlimagen(rs2.getString("urlimagen"));
+//					imagen.setImagenid(rs2.getInt("imagenid"));
+//					imagen.setAnuncioid(rs2.getInt("anuncioid"));
+//					anuncio.add(imagen);
+//				
+//				}
+
+
+				anuncios.add(anuncio);
+			}
+		} catch (SQLException e) {
+			throw new InternalServerException(e.getMessage());
+		}
+
+		finally {
+			try {
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		return anuncios;
+	}
+	
+	
+	
+	
+	
 
 }

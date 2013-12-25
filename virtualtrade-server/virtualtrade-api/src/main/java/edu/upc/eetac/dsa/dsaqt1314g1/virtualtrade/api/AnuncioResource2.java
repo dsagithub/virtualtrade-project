986318267ef5d4e76666538aa1ff266244976fd0
arnau.garcia.casals <arnau193@gmail.com>
2013-12-25 16:04:28 +0100

    Get anuncios orderby precio

--- virtualtrade-server/virtualtrade-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g1/virtualtrade/api/AnuncioResource2.java
+++ virtualtrade-server/virtualtrade-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g1/virtualtrade/api/AnuncioResource2.java
@@ -708,4 +708,120 @@ public class AnuncioResource2 {
 
 	}
 
+	@GET
+	@Path("/orderby")
+	@Produces(MediaType.VIRTUAL_API_ANUNCIO) //precio sera ASC o DESC no es ningún valor numérico es como ordenar
+	public AnuncioCollection getAnunciosPrecio(
+			@QueryParam("offset") String offset,
+			@QueryParam("length") String length,
+			@QueryParam("precio") String precio,@Context Request req) {
+
+		if ((offset == null) || (length == null))
+			throw new BadRequestException(
+					"offset and length are mandatory parameters");
+		int ioffset, ilength;
+		try {
+			ioffset = Integer.parseInt(offset);
+			if (ioffset < 0)
+				throw new NumberFormatException();
+		} catch (NumberFormatException e) {
+			throw new BadRequestException(
+					"offset must be an integer greater or equal than 0.");
+		}
+		try {
+			ilength = Integer.parseInt(length);
+			if (ilength < 1)
+				throw new NumberFormatException();
+		} catch (NumberFormatException e) {
+			throw new BadRequestException(
+					"length must be an integer greater or equal than 1.");
+		}
+
+		AnuncioCollection anuncios = new AnuncioCollection();
+
+		// TODO: Retrieve all stings stored in the database, instantiate one
+		// Sting for each one and store them in the StingCollection.
+		Connection conn = null;
+		Statement stmt = null;
+		Statement stmt1 = null;
+		String sql;
+
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServiceUnavailableException(e.getMessage());
+		}
+
+		try {
+
+			stmt = conn.createStatement();
+
+				sql = "SELECT * FROM anuncio ORDER BY precio "+precio+" LIMIT "
+						+ offset + "," + length + "";
+
+
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				Anuncio anuncio = new Anuncio();
+
+				anuncio.setAnuncioid(rs.getInt("anuncioid"));
+				anuncio.setEmail(rs.getString("email"));
+				anuncio.setSubject(rs.getString("subject"));
+				anuncio.setContent(rs.getString("content"));
+				anuncio.setEstado(rs.getBoolean("estado"));
+				anuncio.setPrecio(rs.getInt("precio"));
+				anuncio.setCreation_timestamp(rs
+						.getTimestamp("creation_timestamp"));
+				anuncio.setAtributo1(rs.getString("atributo1"));
+				anuncio.setAtributo2(rs.getString("atributo2"));
+				anuncio.setAtributo3(rs.getString("atributo3"));
+				anuncio.setMarca(rs.getString("marca"));
+
+				try {
+					stmt1 = conn.createStatement();
+					sql = "SELECT * FROM imagen WHERE anuncioid='"
+							+ rs.getString("anuncioid") + "'";
+
+					ResultSet rs1 = stmt1.executeQuery(sql);
+
+					while (rs1.next()) {
+						Imagen imagen = new Imagen();
+						imagen.setImagenid(rs1.getInt("imagenid"));
+						imagen.setUrlimagen(rs1.getString("urlimagen"));
+						imagen.setAnuncioid(rs1.getInt("anuncioid"));
+						imagen.add(VirtualAPILinkBuilder.buildURIImagen(
+								uriInfo, rs1.getString("imagenid"),
+								rs.getString("anuncioid"), rel));
+						anuncio.add(imagen);
+					}
+				} catch (SQLException e) {
+					throw new AnuncioNotFoundException();
+				}
+
+				List<Link> links = new ArrayList<Link>();
+				links.add(VirtualAPILinkBuilder.buildURIAnuncios(uriInfo, rel));
+
+				anuncios.setLinks(links);
+				anuncios.add(anuncio);
+			}
+		} catch (SQLException e) {
+			throw new InternalServerException(e.getMessage());
+		}
+
+		finally {
+			try {
+				stmt.close();
+				stmt1.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		return anuncios;
+
+	}
+
 }

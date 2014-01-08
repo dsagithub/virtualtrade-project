a69fb5a54702f402914f4b261a5da7d783e3d8e9
arnau.garcia.casals <arnau193@gmail.com>
2014-01-08 17:18:46 +0100

    crear anuncio falta que funcione el chined y poner imagenes

--- virtualtrade-server/virtualtrade-auth/src/main/webapp/js/anuncio.js
+++ virtualtrade-server/virtualtrade-auth/src/main/webapp/js/anuncio.js
@@ -1,37 +1,51 @@
 var API_BASE_URL = "http://localhost:8080/virtualtrade-api";
+$(document).ready(function() {
+	$("#atributo_2").chained("#atributo_1");
+	$("#atributo_3").chained("#atributo_2");
+	$("#atributo_4").chained("#atributo_2");
+	
+});
 
 
-$("#button_create").click(function(e) {
+
+$("#button_post_anuncio").click(function(e) {
 	e.preventDefault();
 	create();
 });
+
 function create() {
-	var url = API_BASE_URL + '/user/repos/';
-	var objecte = new Object();
-	objecte.name = $("#name1").val();
-	objecte.description = $("#description1").val();
 	
-	//objecte.homepage = "https://github.com";
-	objecte.private = false;
-	objecte.has_issues=true;
-	objecte.has_wiki=true;
-	objecte.has_downloads=true;
-	var data = JSON.stringify(objecte);
+
+	var url = API_BASE_URL + '/anuncios';
+	var object = new Object();
+	object.subject = $("#subject").val();
+	object.content = $("#content").val();
+	object.precio=$("#precio").val();
+	object.atributo1=$('#atributo_1 :selected').val();
+	object.atributo2=$('#atributo_2 :selected').val();
+	object.atributo3=$('#atributo_3 :selected').val();
+	object.marca=$('#atributo_4 :selected').val();
+	object.estado = true;
+
+	var data = JSON.stringify(object);
 	$.ajax(
 			{
 				url : url,
 				type : 'POST',
 				dataType : "json",
 				crossDomain : true,
-				data : data,
-				
-				beforeSend : function(request) {
-					request.withCredentials = true;
-					request.setRequestHeader("Authorization", "Basic "+ btoa('username:password'));
+				headers : {
+					"Accept" : "application/vnd.virtual.api.anuncio+json",
+					"Content-Type" : "application/vnd.virtual.api.anuncio+json"
 				},
+				data : data,
+				username : 'arnaumail',
+				password : 'arnau',
+				
 			}).done(function(data, status, jqxhr) {
-		$("#create_result").text("Agregado correctamente");
+		$("#create_result").text("Anuncio creado correctamente");
 	}).fail(function(jqXHR, textStatus) {
 		$("#create_result").text("No agregado");
 	});
 }
+

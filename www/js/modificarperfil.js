fbae087e86d1538c34e0be39f524eb9cc94884f3
arnau.garcia.casals <arnau193@gmail.com>
2014-01-26 20:41:40 +0100

    Modificar

--- www/js/modificarperfil.js
+++ www/js/modificarperfil.js
@@ -9,6 +9,49 @@ $(document).ready(function() {
 
 });
 
+$("#button_update_stings").click(function(e) {
+	e.preventDefault();
+	Modificar();
+
+});
+
+function Modificar() {
+
+	var url = API_BASE_URL + '/users/' + user;
+	var object = new Object();
+	object.name = $("#name").val();
+	object.phone = $("#phone").val();
+	object.ciudad = $("#ciudad").val();
+	object.calle = $("#calle").val();
+	object.numero = $("#numero").val();
+	object.piso = $("#piso").val();
+	object.puerta = $("#puerta").val();
+
+	var data = JSON.stringify(object);
+
+	$.ajax(
+			{
+				url : url,
+				type : 'PUT',
+				crossDomain : true,
+				dataType : 'json',
+				headers : {
+					"Accept" : "application/vnd.virtual.api.user+json",
+					"Content-Type" : "application/vnd.virtual.api.user+json"
+				},
+				beforeSend : function(request) {
+					request.withCredentials = true;
+					request.setRequestHeader("Authorization", "Basic "
+							+ btoa(user + ':' + password));
+				},
+			}).done(function(data, status, jqxhr) {
+
+		$("#update_result").text("Datos modificados correctamente");
+	}).fail(function(jqXHR, textStatus) {
+		$("#update_result").text("Error al modificar los datos");
+	});
+}
+
 function getUser() {
 	var url = API_BASE_URL + "/users/" + user;
 	// $("#name").text("");
@@ -43,14 +86,13 @@ function getUser() {
 		$("#numero").val(user.numero);
 		$("#piso").val(user.piso);
 		$("#puerta").val(user.puerta);
-//		console.log(user.name);
-//		console.log(user.phone);
-//		console.log(user.ciudad);
-//		console.log(user.calle);
-//		console.log(user.numero);
-//		console.log(user.piso);
-//		console.log(user.puerta);
-
+		// console.log(user.name);
+		// console.log(user.phone);
+		// console.log(user.ciudad);
+		// console.log(user.calle);
+		// console.log(user.numero);
+		// console.log(user.piso);
+		// console.log(user.puerta);
 
 	}).fail(function() {
 		console.log("error");

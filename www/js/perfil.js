193592d6716fda42147024f1689b5a5e49f1d057
arnau.garcia.casals <arnau193@gmail.com>
2014-01-26 17:55:30 +0100

    cambios

--- www/js/perfil.js
+++ www/js/perfil.js
@@ -3,20 +3,19 @@ var API_BASE_URL = "http://localhost:8080/virtualtrade-api";
 $(document).ready(function() {
 	getUser($.cookie('email'));
 	var password = $.cookie('userpass');
-	
 
 });
 
 $("#button_edit_user").click(function(e) {
 	e.preventDefault();
-	document.location.href='/virtualtrade/modificarperfil.html';
+	document.location.href = '/virtualtrade/modificarperfil.html';
 });
 
 function getUser(email) {
-	//console.log("JSDFJGSDJGSDKGSDK");
+	// console.log("JSDFJGSDJGSDKGSDK");
 	var url = API_BASE_URL + "/users/" + email;
-	$
-			.ajax({
+	$.ajax(
+			{
 				url : url,
 				type : 'GET',
 				crossDomain : true,
@@ -29,75 +28,48 @@ function getUser(email) {
 					request.setRequestHeader("Authorization", "Basic "
 							+ btoa('adminmail' + ':' + 'admin'));
 				},
-			})
-			.done(
-					function(data, status, jqxhr) {
-						var user = JSON.parse(jqxhr.responseText);
-						$(document)
-								.ready(
-										function() {
-											$("#user_result").text("");
-											foto.src=user.foto;
-//											$("#nom").text(user.name);
-											
-//											foto.height=150;
-//											foto.width=150;
-//											
-//											$("#email").text(user.email);
-//											$("#telef").text(user.phone);
-//											$("#ciudad").text(user.ciudad);
-//											$("#calle").text(user.calle);
-//											$("#numero").text(user.numero);
-//											$("#piso").text(user.piso);
-//											$("#puerta").text(user.puerta);
-//											
-//										
-											
-											var $grouplist = $('#user_result');
-											$(
-													'<li>' + "User Email: "
-															+ user.email
-															+ '</li>')
-													.appendTo($grouplist);
-											$(
-													'<li>' + "Nombre: "
-															+ user.name
-															+ '</li>')
-													.appendTo($grouplist);
-											$(
-													'<li>' + "Phone: "
-															+ user.phone
-															+ '</li>')
-													.appendTo($grouplist);
-											$(
-													'<li>' + "Ciudad: "
-															+ user.ciudad
-															+ '</li>')
-													.appendTo($grouplist);
-											$(
-													'<li>' + "Calle: "
-															+ user.calle
-															+ '</li>')
-													.appendTo($grouplist);
-											$(
-													'<li>' + "Numero: "
-															+ user.numero
-															+ '</li>')
-													.appendTo($grouplist);
-											$(
-													'<li>' + "Piso: "
-															+ user.piso
-															+ '</li>')
-													.appendTo($grouplist);
-											$(
-													'<li>' + "Puerta: "
-															+ user.puerta
-															+ '</li>')
-													.appendTo($grouplist);								
-										});
-	
-						
-					}).fail(function() {
-				$("#user_result").text("User no encontrado");
-			});
+			}).done(
+			function(data, status, jqxhr) {
+				var user = JSON.parse(jqxhr.responseText);
+				$(document).ready(
+						function() {
+							$("#user_result").text("");
+							foto.src = user.foto;
+							// $("#nom").text(user.name);
+
+							// foto.height=150;
+							// foto.width=150;
+							//											
+							// $("#email").text(user.email);
+							// $("#telef").text(user.phone);
+							// $("#ciudad").text(user.ciudad);
+							// $("#calle").text(user.calle);
+							// $("#numero").text(user.numero);
+							// $("#piso").text(user.piso);
+							// $("#puerta").text(user.puerta);
+							//											
+							//										
+
+							var $grouplist = $('#user_result');
+							$('<li>' + "User Email: " + user.email + '</li>')
+									.appendTo($grouplist);
+							$('<li>' + "Nombre: " + user.name + '</li>')
+									.appendTo($grouplist);
+							$('<li>' + "Phone: " + user.phone + '</li>')
+									.appendTo($grouplist);
+							$('<li>' + "Ciudad: " + user.ciudad + '</li>')
+									.appendTo($grouplist);
+							$('<li>' + "Calle: " + user.calle + '</li>')
+									.appendTo($grouplist);
+							$('<li>' + "Numero: " + user.numero + '</li>')
+									.appendTo($grouplist);
+							$('<li>' + "Piso: " + user.piso + '</li>')
+									.appendTo($grouplist);
+							$('<li>' + "Puerta: " + user.puerta + '</li>')
+									.appendTo($grouplist);
+						});
+
+			}).fail(function() {
+		$("#user_result").text("User no encontrado");
+	});
 }

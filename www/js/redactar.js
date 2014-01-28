95d29a523193f38f6799f399d95b7d33d40a821b
arnau.garcia.casals <arnau193@gmail.com>
2014-01-28 16:10:11 +0100

    comprobar campos redactar

--- www/js/redactar.js
+++ www/js/redactar.js
@@ -1,49 +1,44 @@
 var API_BASE_URL = "http://localhost:8080/virtualtrade-api";
 
-var destinatario= $.cookie('destinatario');
-var anuncioid= $.cookie('anuncioid');
-var origen= $.cookie('email');
+var destinatario = $.cookie('destinatario');
+var anuncioid = $.cookie('anuncioid');
+var origen = $.cookie('email');
 var password = $.cookie('userpass')
-
 
 $(document).ready(function() {
 
-	
-	if ($.cookie('loggedin')=='nologueado'){
-	  	  $("#logout").hide();
-	      $("#perfil").hide();
-	      $("#anadirnuevo").hide();
-	      $("#buscar").hide();
-	      $("#mensajes").hide();
+	if ($.cookie('loggedin') == 'nologueado') {
+		$("#logout").hide();
+		$("#perfil").hide();
+		$("#anadirnuevo").hide();
+		$("#buscar").hide();
+		$("#mensajes").hide();
 	}
 
-	if ($.cookie('loggedin')=='logueado'){
-	    $("#singup").hide();
-	    $("#signin").hide();
+	if ($.cookie('loggedin') == 'logueado') {
+		$("#singup").hide();
+		$("#signin").hide();
 	}
-	
-	
+
 });
 
-
 $("#logout").click(function(e) {
-    e.preventDefault();
-    console.log("dsag");
-   logout();
+	e.preventDefault();
+	console.log("dsag");
+	logout();
 });
 
 function logout() {
 	$("#logout").hide();
-    $("#perfil").hide();
-    $("#anadirnuevo").hide();
-    $("#buscar").hide();
-    $("#mensajes").hide();
-		 $.removeCookie('email');
-		 $.removeCookie('userpass');
-         $.cookie('loggedin', "nologueado");
-        window.location = "http://localhost/virtualtrade/index.html";
+	$("#perfil").hide();
+	$("#anadirnuevo").hide();
+	$("#buscar").hide();
+	$("#mensajes").hide();
+	$.removeCookie('email');
+	$.removeCookie('userpass');
+	$.cookie('loggedin', "nologueado");
+	window.location = "http://localhost/virtualtrade/index.html";
 }
-
 
 $("#button_send_msg").click(function(e) {
 	e.preventDefault();
@@ -56,13 +51,12 @@ $("#button_cancell").click(function(e) {
 });
 
 function CancelarMensaje() {
-	//FALTA BORRAR cookies
-	document.location.href='/virtualtrade/index.html';
-	
+	// FALTA BORRAR cookies
+	document.location.href = '/virtualtrade/index.html';
+
 	$.removeCookie('destinatario');
 	$.removeCookie('anuncioid');
 }
-
 
 function EnviarMensaje() {
 
@@ -76,36 +70,52 @@ function EnviarMensaje() {
 	console.log(object.anuncioid);
 	console.log(object.subject);
 	console.log(object.content);
-	
-	var data = JSON.stringify(object);
 
-	$.ajax(
-			{
-				url : url,
-				type : 'POST',
-				dataType : "json",
-				crossDomain : true,
-				headers : {
-					"Accept" : "application/vnd.virtual.api.mensaje+json",
-					"Content-Type" : "application/vnd.virtual.api.mensaje+json"
-				},
-				data : data,
-				beforeSend : function(request) {
-					request.withCredentials = true;
-					request.setRequestHeader("Authorization", "Basic "
-							+ btoa('arnaumail' + ':' + 'arnau'));
-				}
-			}).done(function(data, status, jqxhr) {
-		$("#create_result").text("Anuncio creado correctamente");
-		console.log("Mensaje enviado");
-		//FALTA BORRAR COOKIES
-		document.location.href='/virtualtrade/index.html';
-		
-		$.removeCookie('destinatario');
-		$.removeCookie('anuncioid');
-		
-	}).fail(function(jqXHR, textStatus) {
-		$("#create_result").text("No agregado");
-	});
+	if (object.subject == '') {
+
+		alert("El campo subject es obligatorio"); 
+
+	}
+	
+	else if (object.content == '') {
+
+		alert("El campo content es obligatorio"); 
+
+	}
+
+	else {
+		var data = JSON.stringify(object);
+		$
+				.ajax(
+						{
+							url : url,
+							type : 'POST',
+							dataType : "json",
+							crossDomain : true,
+							headers : {
+								"Accept" : "application/vnd.virtual.api.mensaje+json",
+								"Content-Type" : "application/vnd.virtual.api.mensaje+json"
+							},
+							data : data,
+							beforeSend : function(request) {
+								request.withCredentials = true;
+								request.setRequestHeader("Authorization",
+										"Basic "
+												+ btoa('arnaumail' + ':'
+														+ 'arnau'));
+							}
+						}).done(function(data, status, jqxhr) {
+					$("#create_result").text("Anuncio creado correctamente");
+					console.log("Mensaje enviado");
+					// FALTA BORRAR COOKIES
+					document.location.href = '/virtualtrade/index.html';
+
+					$.removeCookie('destinatario');
+					$.removeCookie('anuncioid');
+
+				}).fail(function(jqXHR, textStatus) {
+					$("#create_result").text("No agregado");
+				});
+	}
 
 }

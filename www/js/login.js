9cea0e7662e044cf81379716288db9de979d3eea
roxanagogonea <roxanagogonea@gmail.com>
2014-01-29 10:50:57 +0100

    cambios

--- www/js/login.js
+++ www/js/login.js
@@ -7,42 +7,38 @@ $("#login").click(function(e) {
 
 $(document).ready(function() {
 
-	
-	if ($.cookie('loggedin')=='nologueado'){
-	  	  $("#logout").hide();
-	      $("#perfil").hide();
-	      $("#anadirnuevo").hide();
-	      $("#buscar").hide();
-	      $("#mensajes").hide();
+	if ($.cookie('loggedin') == 'nologueado') {
+		$("#logout").hide();
+		$("#perfil").hide();
+		$("#anadirnuevo").hide();
+		$("#buscar").hide();
+		$("#mensajes").hide();
 	}
 
-	if ($.cookie('loggedin')=='logueado'){
-	    $("#singup").hide();
-	    $("#signin").hide();
+	if ($.cookie('loggedin') == 'logueado') {
+		$("#singup").hide();
+		$("#signin").hide();
 	}
-	
-	
+
 });
 
-
 $("#logout").click(function(e) {
-    e.preventDefault();
-    console.log("dsag");
-   logout();
+	e.preventDefault();
+	console.log("dsag");
+	logout();
 });
 
 function logout() {
 	$("#logout").hide();
-    $("#perfil").hide();
-    $("#anadirnuevo").hide();
-    $("#buscar").hide();
-    $("#mensajes").hide();
-		 $.removeCookie('email');
-		 $.removeCookie('userpass');
-         $.cookie('loggedin', "nologueado");
-        window.location = "http://147.83.7.155/virtualtrade/index.html";
+	$("#perfil").hide();
+	$("#anadirnuevo").hide();
+	$("#buscar").hide();
+	$("#mensajes").hide();
+	$.removeCookie('email');
+	$.removeCookie('userpass');
+	$.cookie('loggedin', "nologueado");
+	window.location = "http://147.83.7.155/virtualtrade/index.html";
 }
-
 
 function getLogin(email, userpass) {
 	var url = "http://147.83.7.155:8080/virtualtrade-api/" + "users/" + email;
@@ -61,23 +57,21 @@ function getLogin(email, userpass) {
 							+ btoa(email + ':' + userpass));
 				}
 			}).done(function(data, status, jqxhr) {
-			var user = JSON.parse(jqxhr.responseText);
-			console.log(user.banned);
-			if(user.banned==false){
-//				$.notify("Logueado!");
-				$.cookie('email', email);
-				$.cookie('userpass', userpass);
-				
-				$.cookie('loggedin', "logueado");
-				
-				window.location = "http://147.83.7.155/virtualtrade/index.html";
-			//	console.log(data);
-			}
-			else{
-				window.location = "http://147.83.7.155/virtualtrade/banned.html";
-			}
-				
-		
+		var user = JSON.parse(jqxhr.responseText);
+		console.log(user.banned);
+		if (user.banned == false) {
+			// $.notify("Logueado!");
+			$.cookie('email', email);
+			$.cookie('userpass', userpass);
+
+			$.cookie('loggedin', "logueado");
+
+			window.location = "http://147.83.7.155/virtualtrade/index.html";
+			// console.log(data);
+		} else {
+			window.location = "http://147.83.7.155/virtualtrade/banned.html";
+		}
+
 	}).fail(function(jqXHR, textStatus) {
 		$.notify("Incorrecto!");
 		console.log(textStatus);
